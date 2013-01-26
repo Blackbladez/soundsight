cd61985afe885b2f0ca83391627d8fb71af16830
Leonard Cremer <Leonard.cremer@gmail.com>
2013-01-26 17:48:30 -0600

    Player Environment Sight

--- Assets/Project/Scripts/Effects/PlayerEnvironment.cs
+++ Assets/Project/Scripts/Effects/PlayerEnvironment.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
 using System.Collections;
+using Assets.Scripts.Singleton;
 
-public class PlayerEnvironment : MonoBehaviour {
+public class PlayerEnvironment : MonoBehaviour
+{
+    public float Proximity = 25;
+    public Color WallColor = Color.white;
+    public Material SightMaterial;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        if(SightMaterial == null)
+            Debug.LogError("Sight Material has nothing attached!");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+	    Vector3 forward = transform.TransformDirection(Vector3.forward);
+        if(Physics.Raycast(transform.position, forward, Proximity))
+        {
+            //SightMaterial.SetColor("_Color", Color.Lerp(WallColor, Color.black, Mathf.Cos(Time.time * Player.Instance)));
+        }
 	}
 }
